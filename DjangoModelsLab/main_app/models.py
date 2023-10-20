bc249056f3c04660d7e08369439fbf7bd40b2705
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-10-20 18:12:16 +0300

    set up database

--- DjangoModelsLab/main_app/models.py
+++ DjangoModelsLab/main_app/models.py
@@ -5,7 +5,39 @@ from django.db import models
 
 
 class MyBook(models.Model):
-    name = models.TextField(
-        max_length=49,
-        unique=True,
+    AUTH_CHOICES = (
+        ('Stephen King', 'Stephen King'),
+        ('Dan Brown', 'Dan Brown'),
+        ('James Patterson', 'James Patterson'),
+        ('Leo Tolstoy', 'Leo Tolstoy'),
+        ('John Grisham', 'John Grisham'),
     )
+
+    fist_name = models.CharField(
+        max_length=10,
+        unique=True,
+        default='Ivan',
+    )
+
+    last_name = models.CharField(
+        max_length=10,
+        unique=True,
+        default='Ivanov',
+
+    )
+
+    auth = models.TextField(
+        max_length=30,
+        unique=True,
+        default='Stephen King',
+    )
+
+    genre = models.CharField(
+        max_length=40,
+        choices=AUTH_CHOICES,
+        default='Stephen King',
+
+    )
+
+    def __str__(self):
+        return self.auth
