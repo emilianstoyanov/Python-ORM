8fd998dd710eede8b1e419e92bed990e944dbf59
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-10-20 20:26:21 +0300

    database setup

--- MigrationsAndDjangoAdminLab/orm_skeleton/settings.py
+++ MigrationsAndDjangoAdminLab/orm_skeleton/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,13 +79,15 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "",
-        "USER": "",
-        "PASSWORD": "",
-        "HOST": "",
-        "PORT": "",
+        "NAME": config('DATABASES_NAME'),
+        "USER": config('DATABASES_USER'),
+        "PASSWORD": config('DATABASES_PASSWORD'),
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
+
+
 
 
 # Password validation
