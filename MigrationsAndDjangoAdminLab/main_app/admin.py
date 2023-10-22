fc600586283d7f6c7d9e3d57cdcc83f50914a089
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-10-22 20:10:14 +0300

    added fieldsets

--- MigrationsAndDjangoAdminLab/main_app/admin.py
+++ MigrationsAndDjangoAdminLab/main_app/admin.py
@@ -7,3 +7,8 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'category', 'price', 'created_on', 'supplier']
     list_filter = ['category', 'supplier']
     search_fields = ['name', 'category', 'supplier']
+    fieldsets = (
+        ('General Information', {
+            'fields': ('name', 'description', 'price', 'barcode')}),
+        ('Categorization', {'fields': ('category', 'supplier')}),
+    )
