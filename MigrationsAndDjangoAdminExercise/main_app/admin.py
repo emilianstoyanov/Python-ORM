4564b95e0f8bc7c5698e482117571585ead6b543
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-10-23 20:14:39 +0300

    added studentAdmin model

--- MigrationsAndDjangoAdminExercise/main_app/admin.py
+++ MigrationsAndDjangoAdminExercise/main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import EventRegistration
+from .models import EventRegistration, Student
 
 
 @admin.register(EventRegistration)
@@ -7,3 +7,22 @@ class EventRegistrationAdmin(admin.ModelAdmin):
     list_display = ['event_name', 'participant_name', 'registration_date']
     list_filter = ['event_name', 'participant_name', 'registration_date']
     search_fields = ['event_name', 'participant_name', 'registration_date']
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'age', 'grade']
+    list_filter = ['date_of_birth', 'age', 'grade']
+    search_fields = ['first_name']
+
+    fieldsets = (
+        ('Personal Information', {
+            'fields': ('first_name', 'last_name', 'age', 'date_of_birth')
+        }),
+
+        ('Academic Information', {
+            'fields': ('grade',),
+            'classes': ('collapse',)
+
+        })
+    )
--- MigrationsAndDjangoAdminExercise/main_app/models.py
+++ MigrationsAndDjangoAdminExercise/main_app/models.py
@@ -28,3 +28,24 @@ class EventRegistration(models.Model):
 
     def __str__(self):
         return f"{self.participant_name} - {self.event_name}"
+
+
+class Student(models.Model):
+    first_name = models.CharField(
+        max_length=50,
+    )
+
+    last_name = models.CharField(
+        max_length=50,
+    )
+
+    age = models.PositiveIntegerField()
+
+    grade = models.CharField(
+        max_length=10,
+    )
+
+    date_of_birth = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
