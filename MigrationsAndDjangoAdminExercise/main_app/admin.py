18168f2de6c6bcb90c2c85b05e015b1dad5a0a40
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-10-23 20:04:05 +0300

    added EventRegistration model

---
+++ MigrationsAndDjangoAdminExercise/main_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import EventRegistration
+
+
+@admin.register(EventRegistration)
+class EventRegistrationAdmin(admin.ModelAdmin):
+    list_display = ['event_name', 'participant_name', 'registration_date']
+    list_filter = ['event_name', 'participant_name', 'registration_date']
+    search_fields = ['event_name', 'participant_name', 'registration_date']
--- MigrationsAndDjangoAdminExercise/main_app/models.py
+++ MigrationsAndDjangoAdminExercise/main_app/models.py
@@ -1,8 +1,30 @@
 from django.db import models
 
+
 class Shoe(models.Model):
     brand = models.CharField(
         max_length=25,
     )
 
     size = models.PositiveIntegerField()
+
+
+class UniqueBrands(models.Model):
+    brand_name = models.CharField(
+        max_length=25
+    )
+
+
+class EventRegistration(models.Model):
+    event_name = models.CharField(
+        max_length=60,
+    )
+
+    participant_name = models.CharField(
+        max_length=50
+    )
+
+    registration_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.participant_name} - {self.event_name}"
