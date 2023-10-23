ad462214e4610928c013740077572d438015c340
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-10-23 18:48:48 +0300

    added shoe model

--- MigrationsAndDjangoAdminExercise/main_app/models.py
+++ MigrationsAndDjangoAdminExercise/main_app/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
+class Shoe(models.Model):
+    brand = models.CharField(
+        max_length=25,
+    )
 
-class Product(models.Model):
-    name = models.CharField(max_length=500)
-    description = models.TextField(null=True, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.CharField(max_length=100, null=True, blank=True)
-    supplier = models.CharField(max_length=150, null=True, blank=True)
+    size = models.PositiveIntegerField()
