cc7b841c5796a29896dddca2aa088f5f331ba90e
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-10-24 20:34:28 +0300

    create empty migration and create func for update and reverse delivery

--- MigrationsAndDjangoAdminExercise/main_app/models.py
+++ MigrationsAndDjangoAdminExercise/main_app/models.py
@@ -107,3 +107,55 @@ class Smartphone(models.Model):
         default="empty",
 
     )
+
+
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ("P", "Pending"),
+        ("Cpd", "Completed"),
+        ("Cnd", "Cancelled"),
+    )
+
+    product_name = models.CharField(
+        max_length=30,
+    )
+
+    customer_name = models.CharField(
+        max_length=100,
+    )
+
+    order_date = models.DateField()
+
+    status = models.CharField(
+        max_length=30,
+        choices=STATUS_CHOICES,
+
+    )
+
+    amount = models.PositiveIntegerField(
+        default=1,
+    )
+
+    product_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    total_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+    )
+
+    warranty = models.CharField(
+        max_length=100,
+        default="No warranty",
+    )
+
+    delivery = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return f"Order #{self.pk} - {self.customer_name}"
