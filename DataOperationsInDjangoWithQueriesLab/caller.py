0a21527b581f82f0e8e205eaa3ddfabf8cf457c7
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-10-25 14:29:19 +0300

    creating multiple records in the database

--- DataOperationsInDjangoWithQueriesLab/caller.py
+++ DataOperationsInDjangoWithQueriesLab/caller.py
@@ -11,6 +11,32 @@ from main_app.models import Student
 # Create and check models
 # Run and print your queries
 
-print(Student.objects.get(student_id='1'))
-
+# print(Student.objects.get(student_id='1'))
 # print(type(Student.objects.all()))
+
+all_students = [['FC5205', 'John', 'Doje', '1996-05-10', 'johhn.doe@university.com'],
+                ['FC5206', 'John', 'Doge', '1945-05-10', 'jyohn.doe@university.com']
+                ]
+
+
+def add_students():
+    for student in all_students:
+        new_employee = Student(student_id=student[0],
+                               first_name=student[1],
+                               last_name=student[2],
+                               birth_date=student[3],
+                               email=student[4],
+                               )
+        new_employee.save()
+
+        # or
+    # Student.objects.create(
+    #     student_id='FC5205',
+    #     first_name='John',
+    #     last_name='Doje',
+    #     birth_date='1996-05-10',
+    #     email='johhn.doe@university.com',
+    # )
+
+
+add_students()
