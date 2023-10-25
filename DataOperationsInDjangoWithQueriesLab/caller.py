687ae0ad7bc66f3b37d395a054c929a7ab5fc560
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-10-25 14:10:44 +0300

    create and register model

--- DataOperationsInDjangoWithQueriesLab/caller.py
+++ DataOperationsInDjangoWithQueriesLab/caller.py
@@ -5,6 +5,12 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
+from main_app.models import Student
+
 # Import your models
 # Create and check models
 # Run and print your queries
+
+print(Student.objects.get(student_id='1'))
+
+# print(type(Student.objects.all()))
