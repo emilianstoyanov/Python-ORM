d6587aa5c49a1112be0ad53fc93ef2aa434590ae
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-10-29 13:56:02 +0200

    added debug logger

--- WorkingWithQueriesInDjangoLab/orm_skeleton/settings.py
+++ WorkingWithQueriesInDjangoLab/orm_skeleton/settings.py
@@ -122,3 +122,20 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler'}},
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        }}}
