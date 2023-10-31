4e8b5f3396c303acc031288583cdcbfc0a3a7edb
Emilian Stoyanov <e.stoyanov@infra.bg>
2023-10-31 11:58:43 +0200

    start new project Working With Queries In Django Exercise

---
+++ WorkingWithQueriesInDjangoExercise/caller.py
@@ -0,0 +1,10 @@
+import os
+import django
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ready_to_use_django_skeleton.settings")
+django.setup()
+
+# Import your models
+# Create and check models
+# Run and print your queries
