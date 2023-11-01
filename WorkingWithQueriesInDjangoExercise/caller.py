b281df532efc2a9e8bf648f7d0ff1b017fe1280d
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-11-01 12:13:28 +0200

    create new models and functions

--- WorkingWithQueriesInDjangoExercise/caller.py
+++ WorkingWithQueriesInDjangoExercise/caller.py
@@ -1,12 +1,15 @@
 import os
+from typing import List
+
 import django
+
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import ArtWorkGallery
-
+from main_app.models import ArtWorkGallery, Laptop
+from django.db.models import Case, When, Value, F
 
 def show_highest_rated_art() -> str:
     best_artwork = ArtWorkGallery.objects.order_by('-rating', 'id').first()
@@ -25,3 +28,41 @@ def delete_negative_rated_arts() -> None:
     ArtWorkGallery.objects.filter(rating__lt=0).delete()
 
 
+def show_the_most_expensive_laptop() -> str:
+    best_laptop = Laptop.objects.order_by('-price', 'id').first()
+
+    return f"{best_laptop.brand} is the most expensive laptop available for {best_laptop.price}$!"
+
+
+def bulk_create_laptops(*args: List[Laptop]) -> None:
+    Laptop.objects.bulk_create(*args)
+
+
+def update_to_512_GB_storage() -> None:
+    # Laptop.objects.filter(Q(brand='Lenovo') | Q(brand='Asus')).update(storage=512)
+    Laptop.objects.filter(brand_in=['Lenovo', 'Asus']).update(storage=512)
+
+
+def update_to_16_GB_storage() -> None:
+    Laptop.objects.filter(brand_in=['Apple', 'Dell', 'Acer']).update(storage=16)
+
+
+def update_operation_systems():
+    Laptop.objects.update(
+        operation_system=Case(
+            When(brand=['Asus'], then=Value('Windows')),
+            When(brand=['Apple'], then=Value('MacOS')),
+            When(brand=['Dell', 'Acer'], then=Value('Linux')),
+            When(brand=['Lenovo'], then=Value('Chrome OS')),
+            default=F('operation_system')
+        )
+    )
+
+    # Laptop.objects.filter(brand=['Asus']).update(operation_system='Windows')
+    # Laptop.objects.filter(brand=['Apple']).update(operation_system='MacOS')
+    # Laptop.objects.filter(brand__in=['Dell', 'Acer']).update(operation_system='Linux')
+    # Laptop.objects.filter(brand=['Lenovo']).update(operation_system='Chrome OS')
+
+
+def delete_inexpencive_laptops() -> None:
+    Laptop.objects.filter(price__lt=1200).delete()
--- WorkingWithQueriesInDjangoExercise/main_app/models.py
+++ WorkingWithQueriesInDjangoExercise/main_app/models.py
@@ -18,6 +18,51 @@ class ArtWorkGallery(models.Model):
     )
 
 
+class Laptop(models.Model):
+    BRAND_CHOICES = (
+        ('Asus', 'Asus'),
+        ('Acer', 'Acer'),
+        ('Apple', 'Apple'),
+        ('Lenovo', 'Lenovo'),
+        ('Dell', 'Dell'),
+    )
+
+    OPERATION_SYSTEM_CHOICES = (
+        ('Windows', 'Windows'),
+        ('MacOS', 'MacOS'),
+        ('Linux', 'Linux'),
+        ('Chrome OS', 'Chrome OS'),
+
+    )
+
+    brand = models.CharField(
+        max_length=30,
+        choices=BRAND_CHOICES,
+    )
+
+    processor = models.CharField(
+        max_length=100,
+    )
+
+    memory = models.PositiveIntegerField(
+        help_text='Memory in GB',
+    )
+
+    storage = models.PositiveIntegerField(
+        help_text='Storage in GB',
+    )
+
+    operation_system = models.CharField(
+        max_length=30,
+        choices=OPERATION_SYSTEM_CHOICES,
+    )
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+
 class ChessPlayer(models.Model):
     username = models.CharField(max_length=100, unique=True)
     title = models.CharField(max_length=100, default="no title")
