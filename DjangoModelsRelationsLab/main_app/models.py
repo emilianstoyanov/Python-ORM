c44ea5ad7248f0873861491f8f82146b6eaa260c
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-11-02 13:22:03 +0200

    create relation one-to-many

--- DjangoModelsRelationsLab/main_app/models.py
+++ DjangoModelsRelationsLab/main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here
+
+# one
+class Lecturer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+# many
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+    # One-To-Many Relationship
+    lecturer = models.ForeignKey('Lecturer', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
