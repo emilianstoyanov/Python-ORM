ebac390deedc4971bc11fb4cb8a72a11ba95e5d4
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-11-06 19:37:10 +0200

    model inheritance and CHOICES optimization

--- ModelsInheritanceAndCustomizationLab/caller.py
+++ ModelsInheritanceAndCustomizationLab/caller.py
@@ -1,11 +1,7 @@
 import os
 import django
 
-
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models
-# Create and check models
-# Run and print your queries
