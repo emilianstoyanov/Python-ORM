ef9c91db8d6a35f35edd7849cf353d8eee491d33
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-11-09 13:43:38 +0200

    model that validates values

--- ModelsInheritanceAndCustomizationExercise/main_app/models.py
+++ ModelsInheritanceAndCustomizationExercise/main_app/models.py
@@ -1,3 +1,166 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseCharacter(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+
+    description = models.TextField()
+
+    class Meta:
+        abstract = True
+
+
+class Mage(BaseCharacter):
+    elemental_power = models.CharField(
+        max_length=100,
+    )
+
+    spellbook_type = models.CharField(
+        max_length=100,
+    )
+
+
+class Assassin(BaseCharacter):
+    weapon_type = models.CharField(
+        max_length=100,
+    )
+
+    assassination_technique = models.CharField(
+        max_length=100
+    )
+
+
+class DemonHunter(BaseCharacter):
+    weapon_type = models.CharField(
+        max_length=100,
+    )
+
+    demon_slaying_ability = models.CharField(
+        max_length=100,
+    )
+
+
+class TimeMage(Mage):
+    time_magic_mastery = models.CharField(
+        max_length=100,
+    )
+
+    temporal_shift_ability = models.CharField(
+        max_length=100,
+    )
+
+
+class Necromancer(Mage):
+    raise_dead_ability = models.CharField(
+        max_length=100,
+    )
+
+
+class ViperAssassin(Assassin):
+    venomous_strikes_mastery = models.CharField(
+        max_length=100,
+    )
+
+    venomous_bite_ability = models.CharField(
+        max_length=100,
+    )
+
+
+class ShadowbladeAssassin(Assassin):
+    shadowstep_ability = models.CharField(
+        max_length=100,
+    )
+
+
+class VengeanceDemonHunter(DemonHunter):
+    vengeance_mastery = models.CharField(
+        max_length=100,
+    )
+
+    retribution_ability = models.CharField(
+        max_length=100,
+    )
+
+
+class FelbladeDemonHunter(DemonHunter):
+    felblade_ability = models.CharField(
+        max_length=100,
+    )
+
+
+class UserProfile(models.Model):
+    username = models.CharField(
+        max_length=70,
+        unique=True,
+    )
+
+    email = models.EmailField(
+        unique=True,
+    )
+
+    bio = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        to=UserProfile,
+        related_name='sent_messages',
+        on_delete=models.CASCADE,
+    )
+
+    receiver = models.ForeignKey(
+        to=UserProfile,
+        related_name='received_messages',
+        on_delete=models.CASCADE,
+    )
+
+    content = models.TextField()
+
+    timestamp = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    is_read = models.BooleanField(
+        default=False,
+    )
+
+    def mark_as_read(self) -> None:
+        self.is_read = True
+
+    def mark_as_unread(self) -> None:
+        self.is_read = False
+
+    def reply_to_message(self, reply_content: str, receiver: UserProfile) -> object:
+        return Message(
+            sender=self.receiver,
+            receiver=receiver,
+            content=reply_content,
+        )
+
+    def forward_message(self, sender: UserProfile, receiver: UserProfile) -> object:
+        return Message(
+            sender=sender,
+            receiver=receiver,
+            content=self.content
+        )
+
+
+class StudentIDField(models.PositiveIntegerField):
+    def to_python(self, value):
+        try:
+            return int(value)
+        except ValueError:
+            pass
+
+
+class Student(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+
+    student_id = StudentIDField()
